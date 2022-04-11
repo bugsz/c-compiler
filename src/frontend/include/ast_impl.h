9f7c7ced2fa673a326b3d02a8a1773e7e7087b5c
Zhiyuan Pan <zy_pan@zju.edu.cn>
2022-04-11 17:33:03 +0800

    More impl on ast

--- src/frontend/include/ast_impl.h
+++ src/frontend/include/ast_impl.h
@@ -4,18 +4,44 @@
 #define INIT_CHILD_NUM 10
 #define MAX_TOKEN_LEN 50
 
-#define mknode(token, ...) mknode_impl((token), ##__VA_ARGS__, NULL)
+#define mknode(token, ...) mknode_impl(token, ##__VA_ARGS__, NULL)
+#define append_child(parent, ...) append_child_impl(parent, ##__VA_ARGS__, NULL)
 
 typedef struct ast_node_impl ast_node_t;
 typedef ast_node_t* ast_node_ptr;
 
 struct ast_node_impl {
-    char token[MAX_TOKEN_LEN];
+    char token[MAX_TOKEN_LEN]; // type of node
     ast_node_ptr* child;
+    ast_node_ptr parent;
     int n_child;
+
+    int ln, col; // line number and column number
+
+    char name[MAX_TOKEN_LEN]; // name of node
+    char attr[MAX_TOKEN_LEN];
+    union {
+        char op_type[3];
+        char char_val;
+        short short_val;
+        int int_val;
+        long long_val;
+        float float_val;
+        double double_val;
+    } literal_val;
 };
 
+struct sym_tab_impl {
+
+};
+
+#ifdef __cplusplus
 extern "C" {
+#endif
     ast_node_ptr mknode_impl(const char* token, ...);
+    void append_child_impl(ast_node_ptr node, ...);
+    void print_node(ast_node_ptr node);
     void print_ast(ast_node_ptr node);
+#ifdef __cplusplus
 }
+#endif
--- src/frontend/src/ast_impl.c
+++ src/frontend/src/ast_impl.c
@@ -5,38 +5,60 @@
 #include "ast_impl.h"
 
 ast_node_ptr mknode_impl(const char* token, ...) {
-    int _init_child_num = INIT_CHILD_NUM;
     ast_node_ptr node = malloc(sizeof(ast_node_t)), temp;
     strncpy(node->token, token, MAX_TOKEN_LEN);
     node->n_child = 0;
-    node->child = malloc(sizeof(ast_node_ptr) * _init_child_num);
+    node->child = malloc(sizeof(ast_node_ptr) * INIT_CHILD_NUM);
+    node->parent = NULL;
     va_list argp;
     va_start(argp, token);
     while ((temp = va_arg(argp, void*))) {
-        node->n_child++;
-        if(node->n_child > _init_child_num) {
-            _init_child_num *= 2;
-            node->child = realloc(node->child, sizeof(ast_node_ptr) * _init_child_num);
-        }
-        node->child[node->n_child - 1] = temp;
+        append_child(node, temp);
     }
     va_end(argp);
     return node;
 }
 
-void print_ast(ast_node_ptr node) {
-    if(node == NULL) {
-        return;
-    }
-    printf("%s", node->token);
-    if(node->n_child > 0) {
-        printf("(");
-        for(int i = 0; i < node->n_child; i++) {
-            print_ast(node->child[i]);
-            if(i != node->n_child - 1) {
-                printf(",");
-            }
+void append_child_impl(ast_node_ptr node, ...) {
+    unsigned int _size = sizeof(node->child) / sizeof(ast_node_ptr);
+    ast_node_ptr temp;
+    va_list argp;
+    va_start(argp, node);
+    while ((temp = va_arg(argp, void*))) {
+        node->n_child++;
+        if(node->n_child > _size) {
+            _size *= 2;
+            node->child = realloc(node->child, sizeof(ast_node_ptr) * _size);
         }
-        printf(")");
+        node->child[node->n_child - 1] = temp;
+        node->child[node->n_child - 1]->parent = node;
+    }
+    va_end(argp);
+}
+
+static void print_whitespaces(int n) {
+    for(int i = 0; i < n; i++) {
+        printf(" ");
+    }
+}
+
+void print_node(ast_node_ptr node) {
+    printf("%s\n", node->token);
+}
+
+void print_ast(ast_node_ptr node) {
+    if (node == NULL)
+        return;
+    static int tabs = 0;
+    if (node->parent == NULL)
+        tabs = 0;
+    print_node(node);
+    if(node->n_child > 0) {
+        tabs += 2;
+        for (int i = 0; i < node->n_child; i++) {
+            print_whitespaces(tabs);
+            print_ast(node->child[i]);
+        }
+        tabs -= 2;
     }
 }
