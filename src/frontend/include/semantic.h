7967476d68a9d85404d22f10a0e51a3c1aa9f60f
Zhiyuan Pan <zy_pan@zju.edu.cn>
2022-04-14 16:14:52 +0800

    More type check

--- src/frontend/include/semantic.h
+++ src/frontend/include/semantic.h
@@ -1,3 +1,11 @@
+/*
+ * @Author: Pan Zhiyuan
+ * @Date: 2022-04-13 18:47:20
+ * @LastEditors: Pan Zhiyuan
+ * @FilePath: /frontend/include/semantic.h
+ * @Description: 
+ */
+
 #pragma once
 
 #include "ast_impl.h"
@@ -8,8 +16,12 @@ extern "C" {
 #endif
     
     int get_literal_type(const char* literal);
+    int get_bin_expr_type(ast_node_ptr left, ast_node_ptr right);
+    int get_un_expr_type(ast_node_ptr expr);
+    
     void add_symbol(const char* name, int type);
     int get_symbol_type(const char* name);
+    const char* get_function_type(const char* name);
     int is_declared(const char* name);
     void print_sym_tab();
 
