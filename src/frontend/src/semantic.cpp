d4018a834c233e376610314768fbf1564faf5228
Zhiyuan <zy_pan@zju.edu.cn>
2022-04-27 18:25:33 +0800

    implement lvalue check

--- src/frontend/src/semantic.cpp
+++ src/frontend/src/semantic.cpp
@@ -16,181 +16,18 @@
 #include <fstream>
 
 #include "semantic.h"
+#include "symtab_impl.h"
 #include "builtin.h"
 #include "config.h"
 
 using namespace std;
 
-static const char* typeid_deref[] = {
+const char* typeid_deref[] = {
     "void", "char", "short", "int", "long", "float", "double", "string"
 };
 
 static bool warning_flag = false; // disable warning if true
 static void semantic_warning(ast_loc_t loc, const char* fmt, ...);
-
-class SymbolAttr {
-    friend class SymbolTable;
-    
-public:
-    SymbolAttr(int type, bool func) {
-        type_id = type;
-        is_func = func;
-    }
-    ~SymbolAttr() = default;
-
-    void add_param(const int& type_id) {
-        param_type_id.push_back(type_id);
-    }
-
-    int param_nums() {
-        return is_func ? param_type_id.size() : -1;
-    }
-
-    string get_type_name() {
-        if (!is_func) {
-            return typeid_deref[type_id];
-        } else {
-            string ret = string(typeid_deref[type_id]) + " (*) (";
-            for (int i = 0; i < param_type_id.size(); ++i) {
-                ret += typeid_deref[param_type_id[i]];
-                if (i != param_type_id.size() - 1) {
-                    ret += ",";
-                }
-            }
-            ret += ")";
-            return ret;
-        }
-    }
-
-    bool func() {
-        return is_func;
-    }
-
-private:
-    // value type for expr/literal, return type for function
-    int type_id;
-    bool is_func;
-    vector<int> param_type_id;
-};
-
-struct _table {
-    _table(bool global = false, string name = "Annonymous") {
-        parent = nullptr;
-        is_global = global;
-        this->name = is_global ? "TranslationUnit" : name;
-    }
-    unordered_map<string, SymbolAttr> sym_tab_impl;
-    unordered_map<string, string> key_value;
-    string name;
-    bool is_global;
-    _table* parent;
-    vector<_table*> child;
-};
-
-class SymbolTable {
-public:
-    SymbolTable() {
-        init();
-    }
-    ~SymbolTable() = default;
-
-    void init() {
-        global_sym_tab = new _table(true);
-        cur_table = global_sym_tab;
-    }
-    
-    void enter_scope(string name) {
-        _table* new_table = new _table(false, name == "" ? "Annonymous" : name);
-        new_table->parent = cur_table;
-        cur_table->child.push_back(new_table);
-        cur_table = new_table;
-    }
-    
-    void exit_scope() {
-        if (cur_table->parent == nullptr) {
-            throw runtime_error("Cannot exit global scope");
-        }
-        cur_table = cur_table->parent;
-    }
-    
-    void add(const char* name, int type, bool is_func = false, string value = "") {
-        cur_table->sym_tab_impl.insert({ string(name), SymbolAttr(type, is_func) });
-        cur_table->key_value[string(name)] = value;
-    }
-
-    string get_value(const char* name) {
-        auto t = cur_table;
-        while(t->parent != nullptr) {
-            if (t->key_value.find(string(name)) != t->key_value.end()) {
-                string ret = t->key_value[string(name)];
-                if(ret.find("\"") != string::npos) {
-                    ret = ret.substr(1, ret.size() - 2);
-                }
-                return ret;
-            }
-            t = t->parent;
-        }
-        return "";
-    }
-
-    int get(const char* name) {
-        auto iter = cur_table;
-        return get_impl(iter, name);
-    }
-    
-    bool is_redef(const char* name) {
-        auto sym_tab = cur_table->sym_tab_impl;
-        return sym_tab.find(name) != sym_tab.end();
-    }
-    
-    void print() {
-        cout << "------------------------------------------" << endl;
-        cout << "Symbol Table" << endl;
-        cout << "------------------------------------------" << endl;
-        _table* cur = global_sym_tab;
-        print_impl(cur);
-        cout << "------------------------------------------" << endl;
-    }
-
-    _table* get_cur_sym_tab() {
-        return cur_table;
-    }
-
-    _table* get_global_sym_tab() {
-        return global_sym_tab;
-    }
-
-private:
-    _table* global_sym_tab;
-    _table* cur_table;
-    
-    int get_impl(_table* t, const char* name) {
-        if(t->sym_tab_impl.find(name) != t->sym_tab_impl.end()) {
-            return t->sym_tab_impl.find(name)->second.type_id;
-        } else if(t->parent != nullptr) {
-            int ret = get_impl(t->parent, name);
-            if(ret >= 0) return ret;
-        }
-        return -1;
-    }
-
-    void print_impl(_table* t) {
-        static int tabs = 0;
-        if (t->is_global) tabs = 0;
-        for(int i = 0; i < tabs; i++) cout << " ";
-        cout << "Scope: " << hex << &t << " " << t->name
-            << (t->is_global ? " Global" : " Local") << endl;
-        for (auto iter : t->sym_tab_impl) {
-            for(int i = 0; i < tabs; i++) cout << "  ";
-            cout << iter.first << " : " << iter.second.get_type_name() << endl;
-        }
-        for (auto child : t->child) {
-            tabs += 2;
-            print_impl(child);
-            tabs -= 2;
-        }
-    }
-};
 
 static SymbolTable sym_tab;
 
@@ -323,7 +160,7 @@ static void semantic_error(int* n_errs, ast_loc_t loc, const char* fmt, ...) {
     cerr << err_line << endl;
     file.close();
     for (int i = 0; i < loc.last_column - 1; i++) {
-        cerr <<  COLOR_GREEN "~";
+        cerr <<  COLOR_GREEN " ";
     }
     cerr << "^" COLOR_NORMAL << endl;
     (*n_errs)++;
@@ -443,6 +280,14 @@ static string const_fold(int* n_errs, ast_node_ptr left, ast_node_ptr right, str
     return "";
 }
 
+static bool lvalue_validation(ast_node_ptr left, ast_node_ptr right, ast_node_ptr op) {
+    string ltok(left->token);
+    if (ltok != "DeclRefExpr") {
+        return false;
+    }
+    return true;
+}
+
 static void semantic_check_impl(int* n_errs, ast_node_ptr node) {
     if (node == nullptr) {
         return;
@@ -531,12 +376,17 @@ static void semantic_check_impl(int* n_errs, ast_node_ptr node) {
         for (int i = 0;i < node->n_child;i++) {
             semantic_check_impl(n_errs, node->child[i]);
         }
+        if (string(node->val) == "=" && !lvalue_validation(node->child[0], node->child[1], node)) {
+            semantic_error(n_errs, node->pos, "lvalue required as left operand of assignment");
+        }
         int type = expr_type_check(node->child[0], node->child[1], node);
         if (type < 0) {
             semantic_error(n_errs, node->pos, "incompatible types in binary expression");
         } else {
             node->type_id = type;
-            if(string(node->child[0]->token)== "Literal" && string(node->child[1]->token) == "Literal") {
+            if (string(node->val) != "="
+                && string(node->child[0]->token) == "Literal"
+                && string(node->child[1]->token) == "Literal") {
                 string res = const_fold(n_errs, node->child[0], node->child[1], node->val, type);
                 if (!res.empty()) {
                     strcpy(node->val, res.c_str());
