d34b541e5891ecb069acbfe61656d6bfeaaba0a9
mengjunyi <mengjunyi@bytedance.com>
2022-05-12 04:13:00 +0800

    update examples

--- src/visualization/frontend/src/etc/ExampleCode.js
+++ src/visualization/frontend/src/etc/ExampleCode.js
@@ -1,8 +1,36 @@
 export const Examples = [
     {
         id: 1,
-        name: 'Basic',
-        code: `int main() {
+        name: 'Introduction',
+        code: 
+`/*
+A "builtin.h" header file will be added to your code
+automatically, so the following functions and defines
+are always availiable.
+#define NULL 0
+int printf(char *format, ...);
+int sprintf(char *buf, char *format, ...);
+int scanf(char *format,...);
+int strlen(char *str,...);
+char *strcpy(char *destin, char *source);
+char *strncpy(char *dest, char *src, int n);
+char *strdup(char *s);
+char *strcat(char *str1, char *str2);
+char *strset(char *str, char ch);
+char *strnset(char *str, char ch, int n);
+int strcmp(char *str1, char *str2);
+void *memset(void *str, int c, int n);
+int memcmp(void *str1, void *str2, int n);
+void *memcpy(void *dest, void *src, int n);
+int putchar(char ch);
+int puts(char* s);
+int getchar();
+int gets(char* buf);
+void *malloc(int size);
+void free(void* p);
+void exit(int status);
+*/
+int main() {
     printf("hello world!");
     return 0;
 }`
