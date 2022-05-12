cd116bcfa069ef1736c5387e59bfb05b694c2347
mengjunyi <mengjunyi@bytedance.com>
2022-05-12 17:40:03 +0800

    support va_args(experimental feature)

--- src/compatible-header/arg.h
+++ src/compatible-header/arg.h
@@ -2,37 +2,55 @@
 #define __ARG_H
 
 #ifndef _VA_LIST
-typedef __builtin_va_list va_list;
-// struct __va_list_tag {
-//     unsigned int gp_offset;   size 4
-//     unsigned int fp_offset;   size 4
-//     void *overflow_arg_area;  size 8
-//     void *reg_save_area;      size 8
-// }
+typedef char* va_list;
+/*
+Actually it is
+struct __va_list_tag {
+    unsigned int gp_offset;   size 4
+    unsigned int fp_offset;   size 4
+    void *overflow_arg_area;  size 8
+    void *reg_save_area;      size 8
+}
+*/
 #define _VA_LIST
 #endif
-#define va_start(ap, param) __llvm_va_start(ap, param)
-#define va_end(ap)          __llvm_va_end(ap)
 
+#define va_start(ap, param) {ap = malloc(24);__llvm_va_start(ap);}
+#define va_end(ap)          {free(ap);}
+void __llvm_va_start(char* ap);
+
+char va_arg_AE51CE10FCDC4992851[8];                
 /*
-Since every x64 register is 64 bit,=
-and 6 of them can be used to pass params,=
-so the offset is at most 40(may equal)=
+Since every x64 register is 64 bit,
+and 6 of them can be used to pass params,
+so the offset is at most 40(may equal)
 */  
-#define va_arg(ap, type)\
-int gp_offset = *(int *)ap;\
-char * overflow_arg_area = *(long *)(ap+8);\
-char * reg_save_area = *(long *)(ap+16);\
-char buf[sizeof(type)];\
-int size = sizeof(type);\
-if(gp_offset <= 40){\
-    memcpy(buf, reg_save_area, size);\
-    *(int *)ap += size;\ 
-    *(long *)(ap+16) += size;\
-}else{\
-    memcpy(buf, overflow_arg_area, size);\
-        *(long *)(ap+8) += size;\
+#define va_arg(ap, type)                                                 \
+({                                                                       \
+int * gp_offset = ap;                                        \
+int * fp_offset = ap+4;                                      \
+char * overflow_arg_area = (*(long *)(ap+8));                            \
+char * reg_save_area = (*(long *)(ap+16)) + *gp_offset;             \
+char * fp_reg_save_area = (*(long *)(ap+16)) + *fp_offset;   \
+if(isfp(type)){\
+    if(*fp_offset <= 160){                                     \
+        memcpy(va_arg_AE51CE10FCDC4992851, fp_reg_save_area, sizeof(type));     \
+        *fp_offset += 16;                                                     \
+    }else{\
+        memcpy(va_arg_AE51CE10FCDC4992851, overflow_arg_area, sizeof(type)); \
+        *(long *)(ap+8) += 16;\
+    }\
 }\
-*(type *)buf;\
+if(!isfp(type)){                                            \
+    if(*gp_offset <= 40){                               \
+        memcpy(va_arg_AE51CE10FCDC4992851, reg_save_area, sizeof(type));     \
+        *gp_offset += 8;                                                     \
+    }else{\
+        memcpy(va_arg_AE51CE10FCDC4992851, overflow_arg_area, sizeof(type)); \
+        *(long *)(ap+8) += 8;\
+    }\
+};`\
+*(type *)va_arg_AE51CE10FCDC4992851;                                     \
+})
 
 #endif /* __ARG_H */
